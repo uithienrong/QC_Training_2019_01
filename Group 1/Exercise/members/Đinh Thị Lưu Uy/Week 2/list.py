9d7de9c37b194099749265a9929f29ea6d147bcb
LuuUy <luuuy147@gmail.com>
2019-01-18 16:13:14 +0700

    week 2 Dinh Thi Luu Uy

---
+++ Group 1/Exercise/members/Đinh Thị Lưu Uy/Week 2/list.py
@@ -0,0 +1,40 @@
+# Given a list of strings, return a list with the strings
+# in sorted order, except group all the strings that begin with 'x' first.
+# e.g. ['mix', 'xyz', 'apple', 'xanadu', 'aardvark'] yields
+# ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
+import re
+def front_x(words):
+	listx=[]
+	listnotx=[]
+	for i in words:
+		if re.match("x",i):
+			listx.append(i)
+		else:
+			listnotx.append(i)
+		listx.sort()
+		listnotx.sort()
+	return listx+listnotx
+
+
+
+# test() function
+def test(result, expected):
+  if result == expected:
+    output = ' OK '
+  else:
+    output = '  X '
+  print '%s result: %s expected: %s' % (output, repr(result), repr(expected))
+
+
+# main() functions
+def main():
+  print 'front_x'
+  test(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']),
+       ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
+  test(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']),
+       ['xaa', 'xcc', 'aaa', 'bbb', 'ccc'])
+  test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']),
+       ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
+
+       
+main()
---
+++ Group 1/Exercise/members/Đinh Thị Lưu Uy/Week 2/q.py
@@ -0,0 +1,13 @@
+import re
+def front_x(words):
+	words =['mix', 'xyz', 'apple', 'xanadu', 'aardvark']
+  	print (words)
+  	return words
+front_x([])
+list =['mix', 'xyz', 'apple', 'xanadu', 'aardvark']
+list1=['xyz','xanadu']
+list2=['mix']
+list3=['apple','aardvark']
+list4=list1+list2+list3
+list.sort()
+print (list4)
